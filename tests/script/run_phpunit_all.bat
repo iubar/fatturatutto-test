7658cca056cd6cbb0d686d55cbef720b648bece6
Matteo Mirisola <mirimmm94@gmail.com>
2016-05-20 19:25:42 +0200

    remove chrome.exe to shutdown selenium for inutility

--- tests/script/run_phpunit_all.bat
+++ tests/script/run_phpunit_all.bat
@@ -8,4 +8,4 @@ start C:\Users\Matteo\Desktop\selenium\start_phantomjs.bat
 call phpunit -c phpunit_phantomjs.xml
 
 REM stop selenium
-start chrome.exe http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
+start http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
