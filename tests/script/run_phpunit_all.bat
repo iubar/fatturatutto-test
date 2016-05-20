21ad802ed2f699bd12040b3f9272a104a1961cc6
Matteo Mirisola <mirimmm94@gmail.com>
2016-05-20 19:13:10 +0200

    add run_phpunit_all, run_phpunit_phantomjs

---
+++ tests/script/run_phpunit_all.bat
@@ -0,0 +1,11 @@
+REM start selenium
+start C:\Users\Matteo\Desktop\selenium\start_selenium_all.bat
+
+call phpunit -c phpunit_chrome.xml
+call phpunit -c phpunit_marionette.xml
+
+start C:\Users\Matteo\Desktop\selenium\start_phantomjs.bat
+call phpunit -c phpunit_phantomjs.xml
+
+REM stop selenium
+start chrome.exe http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
---
+++ tests/script/run_phpunit_phantomjs.bat
@@ -0,0 +1,8 @@
+REM start selenium
+start C:\Users\Matteo\Desktop\selenium\start_selenium_all.bat
+
+start C:\Users\Matteo\Desktop\selenium\start_phantomjs.bat
+call phpunit -c phpunit_phantomjs.xml
+
+REM stop selenium
+start chrome.exe http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
