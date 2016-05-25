9a3130b5e17aaf39c6a21ae21e9d72b8b9d12b3d
Andrea Borgogelli Avveduti <borgogelli@users.noreply.github.com>
2016-05-25 11:00:56 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-require realpath(__DIR__) . 'vendor/autoload.php';
+// require realpath(__DIR__) . '/vendor/autoload.php';
+require 'tests/vendor/autoload.php';
 
