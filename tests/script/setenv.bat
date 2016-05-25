cdb8d98703ecc8aa3d4098093e5febb5fd92b823
Matteo Mirisola <mirimmm94@gmail.com>
2016-05-25 19:35:45 +0200

    changed SCREENSHOTS_PATH

--- tests/script/setenv.bat
+++ tests/script/setenv.bat
@@ -1,5 +1,5 @@
 SET BROWSER=chrome
-SET SCREENSHOTS_PATH=../../logs/screenshots/
+SET SCREENSHOTS_PATH=%~dp0../../logs/screenshots/
 SET SERVER=http://localhost:4444/wd/hub
 SET /P USERNAME="Please enter the username: "
 SET /P PASSWORD="Please enter the password for %USERNAME%: "
