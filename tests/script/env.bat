7ef5571c5cbf8791112c6d900ccaa01969ea0202
Matteo Mirisola <mirimmm94@gmail.com>
2016-06-01 18:35:47 +0200

    .

--- tests/script/env.bat
+++ tests/script/env.bat
@@ -1,6 +1,11 @@
-SET BROWSER=firefox
+REM USEAGE: chrome, firefox, marionette, phantomjs
+
+SET BROWSER=chrome
 SET SCREENSHOTS_PATH=%~dp0..\..\logs\screenshots\
 SET SELENIUM_SERVER=localhost:4444
 SET SELENIUM_PATH=%UserProfile%\Desktop\selenium
-SET /P FT_USERNAME="Please enter the username: "
-SET /P FT_PASSWORD="Please enter the password for %FT_USERNAME%: "
+REM SET /P FT_USERNAME="Please enter the username: "
+REM SET /P FT_PASSWORD="Please enter the password for %FT_USERNAME%: "
+
+SET FT_USERNAME=mirimmm94@gmail.com
+SET FT_PASSWORD=matteo7
