93f2c1aa1ff2e930ee53feb63050471c3e9023c9
maunigit <mirimmm94@gmail.com>
2016-06-01 18:36:11 +0200

    Update env.bat

--- tests/script/env.bat
+++ tests/script/env.bat
@@ -7,5 +7,3 @@ SET SELENIUM_PATH=%UserProfile%\Desktop\selenium
 REM SET /P FT_USERNAME="Please enter the username: "
 REM SET /P FT_PASSWORD="Please enter the password for %FT_USERNAME%: "
 
-SET FT_USERNAME=mirimmm94@gmail.com
-SET FT_PASSWORD=matteo7
