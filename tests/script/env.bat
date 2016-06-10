f362e2e5e0774c8e63aafb7e03af116dfcfa761f
Matteo Mirisola <mirimmm94@gmail.com>
2016-06-10 16:01:21 +0200

    .

--- tests/script/env.bat
+++ tests/script/env.bat
@@ -4,5 +4,6 @@ SET BROWSER=chrome
 SET SCREENSHOTS_PATH=%~dp0..\..\logs\screenshots\
 SET SELENIUM_SERVER=localhost:4444
 SET SELENIUM_PATH=%UserProfile%\Desktop\selenium
-SET /P FT_USERNAME="Please enter the username: "
-SET /P FT_PASSWORD="Please enter the password for %FT_USERNAME%: "
+
+REM SET /P FT_USERNAME="Please enter the username: "
+REM SET /P FT_PASSWORD="Please enter the password for %FT_USERNAME%: "
