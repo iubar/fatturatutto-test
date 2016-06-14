7d37c732850ecaa6ba1b6ffdfb4d25a815bedab2
Matteo Mirisola <mirimmm94@gmail.com>
2016-06-14 17:59:05 +0200

    .

--- tests/Fatturatutto/Tests/Security/Security.php
+++ tests/Fatturatutto/Tests/Security/Security.php
@@ -11,33 +11,4 @@ use GuzzleHttp\Exception\RequestException;
  * @author Matteo
  *        
  */
-class Security {
-    //http control status code 
-    const FORBIDDEN = 403;
-    const UNAUTHORIZED = 401;
-    const MOVED = 301;
-    
-//     $urls = [$FORBIDDEN => array(
-//         "www.fatturatutto.it/app/logs",
-//         "app.fatturatutto.it/logs",
-//         "www.fatturatutto.it/app/vendor",
-//         "app.fatturatutto.it/vendor"
-//     ),
-//         [$UNAUTHORIZED > array(
-//             "www.dataslang.com/wp-login.php"
-//         )
-//         ];
-
-// foreach ($urls as $error_code => $url) {
-//     $status_code = null;
-//     while ($status_code == null || $status_code == $MOVED) {
-//                 $response = $client->...;
-//                 $status-code = $response->get.....;
-//         if ($status_code == $MOVED) {
-//                     $url = $response->get.....;
-//         }
-//     }
-    
-//     assertEqual($error_code, $status_code);
-// }
-}
+class Security {}
