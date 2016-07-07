98c8f4d2642e77f08b37520282d9c74b2222796c
danielemontesi <daniele.montesi@gmail.com>
2016-07-07 13:38:48 +0200

    .

--- tests/build/dos/config.bat
+++ tests/build/dos/config.bat
@@ -4,8 +4,7 @@ REM USEAGE: chrome, firefox, marionette, phantomjs
 SET BROWSER=chrome
 SET SCREENSHOTS_PATH=%~dp0..\..\..\logs\screenshots
 SET SELENIUM_SERVER=localhost:4444
-SET SELENIUM_PATH=%UserProfile%\Desktop\selenium
-
+SET SELENIUM_PATH=C:\Users\Daniele\PortableApps\MyApps\selenium
 SET APP_HOST=fatturatutto.it
 SET /P APP_USERNAME="Please enter the username: "
 SET /P APP_PASSWORD="Please enter the password for %APP_USERNAME%: "
