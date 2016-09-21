dc46545ee43bef786ba791df8336153cfb318bd2
danielemontesi <daniele.montesi@gmail.com>
2016-09-21 12:15:10 +0200

    .

--- tests/Fatturatutto/Tests/Security/SecurityTest.php
+++ tests/Fatturatutto/Tests/Security/SecurityTest.php
@@ -80,8 +80,8 @@ class SecurityTest extends RestApi_TestCase {
                             'verify' => false,  // Ignora la verifica dei certificati SSL (obbligatorio per accesso a risorse https)
                                                 // @see: http://docs.guzzlephp.org/en/latest/request-options.html#verify-option
                             'curl' => [
-                                CURLOPT_SSLVERSION => 3
-                                //CURLOPT_SSLVERSION => CURL_SSLVERSION_DEFAULT,
+                                // CURLOPT_SSLVERSION => 3
+                                CURLOPT_SSLVERSION => CURL_SSLVERSION_DEFAULT,
                                 // CURLOPT_SSL_VERIFYPEER => false
                             ],
                         ]);
