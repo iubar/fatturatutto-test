a0f24be51449d680fe27a462d4c5ae6016c9b1cf
danielemontesi <daniele.montesi@iubar.it>
2017-02-03 21:06:44 +0100

    .
    .

--- build/dos/config.bat
+++ build/dos/config.bat
@@ -4,7 +4,7 @@ REM USEAGE: chrome, firefox, marionette, phantomjs
 SET BROWSER=chrome
 SET SCREENSHOTS_PATH=%~dp0..\..\logs\screenshots
 SET SELENIUM_SERVER=localhost:4444
-SET SELENIUM_PATH=C:\Users\Daniele\PortableApps\MyApps\selenium
+SET SELENIUM_PATH=\\apollo\doc\dev\selenium
 SET APP_HOST=fatturatutto.it
 SET /P APP_USERNAME="Please enter the username: "
 SET /P APP_PASSWORD="Please enter the password for %APP_USERNAME%: "
--- tests/Fatturatutto/Tests/Api/FatturaTuttoApiTest.php
+++ tests/Fatturatutto/Tests/Api/FatturaTuttoApiTest.php
@@ -12,7 +12,7 @@ use Iubar\Tests\RestApi_TestCase;
  *  Esempio di richiesta GET: http://www.fatturatutto.it/app/api/test/fattura-esempio?user=andrea@iubar.it&ts=2016-04-01T17%3A29%3A23%2B02%3A00&hash=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
  *
  */
-class FatturaTuttoApiTest extends RestApi_TestCase {
+class FatturatuttoApiTest extends RestApi_TestCase {
 
     private $url = 'https://app.fatturatutto.it/api/test/fattura-esempio/1'; // eg: limit=10
 
